e9c13d2b293dbead60ff4a4ba96779095545be59
David Dixon <David.Dixon@visionpointllc.com>
2020-07-16 11:10:44 -0400

    Upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# CTS Practice Exams
+ Website to help prepare you for the CTS Exam.
---
+++ indexscript.js
@@ -0,0 +1,50 @@
+var takeexambutton = document.getElementById("BeginExam");
+takeexambutton.style.display = "none";
+let selectedExam = "None";
+let loadedExam = "None";
+
+
+function selectExam(item)
+{
+	selectedExam = item.id;
+	if(selectedExam.substring(2) != "0")
+	{
+		document.getElementById("CurrentExam").innerHTML = `Exam Selected: Chapter Exam ${selectedExam.substring(2)}`;
+	}
+	else
+	{
+		document.getElementById("CurrentExam").innerHTML = 'Exam Selected: Book Exam';
+	}
+	takeexambutton.style.display = "inline-block";
+	
+}
+
+function loadExam()
+{
+	if(loadedExam != selectedExam)
+	{
+		loadedExam = selectedExam;
+		var myData = loadedExam;
+		localStorage.setItem('objectToPass', myData);
+	}
+}
+
+document.querySelectorAll(".SelectExam").forEach(item => {
+	item.addEventListener('click', event => { 
+		selectExam(item);
+		console.log(item.getElementsByTagName("INPUT")); 
+		item.getElementsByTagName("INPUT")[0].checked = true;
+	})
+})
+
+takeexambutton.addEventListener('click', loadExam);
+
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
